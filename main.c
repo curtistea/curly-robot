84f22bf865288fb70e0314905f2a506bf7958db2
curtistea <122052623+curtistea@users.noreply.github.com>
2023-01-22 20:02:32 +0300

    Update main.c
    calculator

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() 
 {
-   int a;
-   printf("Enter a number");
+   int a, b;
+   printf("Enter two numbers");
    scanf("%d", &a);
-   printf("Hello, %d!", a);
+   scanf("%d", &b);
+   printf("Hello, %d + %d = %d!", a, b, a+b);
    return 0;
 }
